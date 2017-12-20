7d4618a65a629dec23d45d6e0c000c02c7922397
Chris Guikema <chris.guikema@dornerworks.com>
2017-12-20 11:23:59 -0500

    Updated the KERNEL_RESERVED_START to a 10-bit limit that doesn't overlap with the kernel address space.
    Change-Id: I97cbc191f3f510d1d2d04c1e7698b4a2a7f7ca2f

--- libsel4utils/sel4_arch_include/aarch64/sel4utils/sel4_arch/vspace.h
+++ libsel4utils/sel4_arch_include/aarch64/sel4utils/sel4_arch/vspace.h
@@ -3,18 +3,28 @@
  * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
  * ABN 41 687 119 230.
  *
+ * Copyright 2018, DornerWorks
+ *
  * This software may be distributed and modified according to the terms of
  * the BSD 2-Clause license. Note that NO WARRANTY is provided.
  * See "LICENSE_BSD2.txt" for details.
  *
- * @TAG(DATA61_BSD)
+ * @TAG(DATA61_DORNERWORKS_BSD)
  */
 
 #pragma once
 
+#ifndef CONFIG_ARM_HYPERVISOR_SUPPORT
 #define KERNEL_RESERVED_START 0x00007ffffffff000
+#else
+#define KERNEL_RESERVED_START 0x0000007ffffff000
+#endif
+
 #define VSPACE_MAP_PAGING_OBJECTS 5
 
 #define VSPACE_LEVEL_BITS 9
+#ifndef CONFIG_ARM_HYPERVISOR_SUPPORT
 #define VSPACE_NUM_LEVELS 4
-
+#else
+#define VSPACE_NUM_LEVELS 3
+#endif
